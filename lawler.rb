01fb11c289308800e460f7fef43197916549c5ad
markfranciose <mark.franciose@gmail.com>
2017-11-01 16:51:03 -0500

    add in skeleton of the app

---
+++ lawler.rb
@@ -0,0 +1,86 @@
+module LawlerIpsum
+
+	def self.words(number = 20)	
+		# loader = Loader.new
+		array = [
+				"Macho Man",
+				"Bobby Heenan",
+				"Crush",
+				"Dingo Warrior",
+				"Lodi",
+				"the Yet-ay",
+				"Shockmaster",
+				"Ohhh.... yeaaaahhh",
+				"headbut"
+		]
+
+		output = ""
+
+		number.times do 
+			output += array.sample + " "
+		end
+
+		output
+
+	end
+
+	@@verbs = [
+		"smacks",
+		"hits",
+		"bams",
+		"wallops",
+		"strikes",
+		"counters",
+		"reaches deep, deep down and lays out"
+	]
+	
+	def self.better_words(number = 100)
+		lines = []
+		File.open(File.dirname(__FILE__) << '/wrestlers.txt').each_line do |line|
+			lines << line.gsub!(/\n/, " ")
+		end
+
+		lines.shuffle.join("")
+				
+	end
+
+	def self.pick_wrestler
+		wrestlers = []
+		File.open(File.dirname(__FILE__) << '/wrestlers.txt').each_line do |line|
+			wrestlers << line.gsub(/\n/, "")
+		end
+
+		wrestlers.sample
+	end
+	
+	def self.pick_move
+		moves = []
+		File.open(File.dirname(__FILE__) << '/moves.txt').each_line do |line|
+			moves << line.gsub(/\n/, "")
+		end
+
+		moves.sample
+	end
+	
+	def self.yet_better()
+		pick_wrestler + " " + @@verbs.sample + " " + pick_wrestler + " with a " + pick_move + "!"
+		# select from verb
+		# select from wrestler name array
+		# with a
+		# select from moves
+	end
+
+
+
+	private
+
+	class Loader
+		
+	end
+	
+
+
+end
+
+
+puts LawlerIpsum.yet_better
