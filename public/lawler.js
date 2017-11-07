b8b74d6022884021a92aacea475f38331613bb4c
markfranciose <mark.franciose@gmail.com>
2017-11-06 19:12:43 -0600

    fix bugs w/ no input or non-numeric input

--- public/lawler.js
+++ public/lawler.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
 	let generateButton = document.getElementById("generate-button");
 	let showBox = document.getElementById("show-box");
-	let copyButton = document.getElementById("copy-button")	
+	let copyButton = document.getElementById("copy-button");
 
 	generateButton.addEventListener("click", function(){
 		let xhr = new XMLHttpRequest();
 		let selected = document.querySelector("input[name='which-text']:checked").value
-		console.log(selected)
-		let number = document.getElementById(selected + '-number').value
-		console.log('/' + selected + '/' + number)
+		
+		// return 1 para or sent if no number chosen
+		let number = document.getElementById(selected + '-number').value || "1" 
+		// return 1 para or sent if non-num chars entered
+		if ( (/\D/).test(number) ) {
+			number = "1"; 
+		}
+			
 		xhr.open('GET', '/' + selected + '/' + number);
 		xhr.send(null);
 		xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			showBox.value = xhr.responseText;
-		}	
+			if (xhr.readyState === 4) {
+				showBox.value = xhr.responseText;
+			}	
 		}
 	})
 
